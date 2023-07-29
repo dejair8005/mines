c3f5e210c187cb77f076842d5a71c631bacd0a20
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-07-29 20:06:22 -0300

    Commit

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native' // Fixed import
 
 import params from './src/params'
+import Field from './src/components/Field'
 
 export default class App extends Component {
     render() {
@@ -17,6 +18,18 @@ export default class App extends Component {
           <Text style={styles.instructions}>Tamanho da grade:
             {params.getRowsAmount()}x{params.getColumnsAmount()}
           </Text>
+
+          <Field />
+          <Field opened/>
+          <Field opened nearMines={1} />
+          <Field opened nearMines={2} />
+          <Field opened nearMines={3} />
+          <Field opened nearMines={7} />
+          <Field mined />
+          <Field mined opened/>
+          <Field mined opened exploded/>
+          <Field flagged />
+          <Field flagged opened/>
         </View>
       );
     }
