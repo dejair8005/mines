85e202fc6dc812f9b9f874c5ba1c23a2292bcda4
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-08-10 14:32:34 -0300

    Last edition Course

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native' // Fixed import
-
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Alert} from 'react-native';
 import params from './src/params'
-import Field from './src/components/Field'
 import MineField from './src/components/MineField'
+import Header from './src/components/Header'
+import LevelSelection from './src/screens/LevelSelection'
 import {
-  createMinedBoard
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+  invertFlag,
+  flagsUsed
 } from './src/Logic'
 
 export default class App extends Component {
@@ -20,52 +21,79 @@ export default class App extends Component {
     super(props)
     this.state = this.createState()
   }
-  
+
   minesAmount = () => {
     const cols = params.getColumnsAmount()
     const rows = params.getRowsAmount()
-    return Math.ceil( cols * rows * params.difficultLevel)
+    return Math.ceil(cols * rows * params.difficultLevel)
   }
-
 
   createState = () => {
-     const cols = params.getColumnsAmount()
-     const rows = params.getRowsAmount()
-     return {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
       board: createMinedBoard(rows, cols, this.minesAmount()),
-     }
+      won: false,
+      lost: false,
+      showLevelSelection: false,
+    }
   }
 
-    render() {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.welcome}>Iniciando o Mines!</Text>
-          <Text style={styles.instructions}>Tamanho da grade:
-            {params.getRowsAmount()}x{params.getColumnsAmount()}
-          </Text>
-          <View style={styles.board}>
-            <MineField board={this.state.board}/>
-          </View>
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
 
-          {/*<Field />
-          <Field opened/>
-          <Field opened nearMines={1} />
-          <Field opened nearMines={2} />
-          <Field opened nearMines={3} />
-          <Field opened nearMines={7} />
-          <Field mined />
-          <Field mined opened/>
-          <Field mined opened exploded/>
-          <Field flagged />
-      <Field flagged opened/>*/} 
-
-          
-        </View>
-      );
+    if (lost) {
+      showMines(board)
+      Alert.alert('Perdeeeeu!', 'Que buuuurro!')
     }
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você Venceu!')
+    }
+
+    this.setState({ board, lost, won })
+  }
+
+  onSelectField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    invertFlag(board, row, column)
+    const won = wonGame(board)
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você Venceu!')
+    }
+
+    this.setState({ board, won })
+  }
+
+  onLevelSelected = level => {
+    params.difficultLevel = level
+    this.setState(this.createState())
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <LevelSelection isVisible={this.state.showLevelSelection}
+          onLevelSelected={this.onLevelSelected}
+          onCancel={() => this.setState({ showLevelSelection: false })} />
+        <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)}
+          onNewGame={() => this.setState(this.createState())} 
+          onFlagPress={() => this.setState({ showLevelSelection: true })} />
+        <View style={styles.board}>
+          <MineField board={this.state.board} 
+            onOpenField={this.onOpenField}
+            onSelectField={this.onSelectField} />
+        </View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({ // Fixed StylesSheet with StyleSheet
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-end'
